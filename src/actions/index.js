2554d7133f25782c88f8dcf21b82a762319e4c8c
Arash <arashdai@gmail.com>
2017-01-25 17:26:43 -0800

    form work

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,9 +16,10 @@ export function submitForm(props){
 
     const request = axios.post('/contact',contactInfo)
     .then(()=>{
-        if(request.PromiseValue.data === 'success'){ console.log('IT WORKED')}
-        else if(request.PromiseValue.onerror.length > 0 ){ console.log('Something Went Wrong')}
+        if(request.value.data === 'success'){ console.log('IT WORKED')}
+        else if(request.value.onerror.length > 0 ){ console.log('Something Went Wrong')}
     })
+    console.log(request)
     //right here need to handle success and error response from server
     //  if successful I want to : pop a modal that says you were successful
     //  if failed I want to : pop a modal that says your message failed please try again
